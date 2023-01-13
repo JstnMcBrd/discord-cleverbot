5bbadc7a6822b5da7ee28e5b9777d1d4736fd74d
Justin McBride <jstnmcbrd@outlook.com>
2023-01-13 01:17:26 -0700

    Convert parameters.js to TypeScript

---
+++ src/parameters.ts
@@ -0,0 +1,30 @@
+/**
+ * When this code was last changed.
+ */
+export const lastUpdated = new Date(2022, 8, 30, 3, 30);
+// Year, month (0-11), day of month, hour (0-23), minutes
+
+/**
+ * How fast the bot sends messages (in characters per second).
+ */
+export const typingSpeed = 6;
+
+/**
+ * The colors that the console output should use.
+ */
+export const debugTheme = {
+	system: ['green'],
+	warning: ['yellow'],
+	error: ['red'],
+	info: ['gray'],
+};
+
+/**
+ * The hexadecimal values of colors to be used for embeds.
+ */
+export const embedColors = {
+	error: 0xFF0000,
+	info: 0xCFD8FF,
+	whitelist: 0x00FF00,
+	unwhitelist: 0xFFF900,
+};
