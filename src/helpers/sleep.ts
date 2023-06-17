6c5de209cf5f9571a707ce4314974291ab3c6d96
Justin McBride <jstnmcbrd@outlook.com>
2023-06-16 18:41:48 -0600

    Add sleep method

---
+++ src/helpers/sleep.ts
@@ -0,0 +1,9 @@
+/**
+ * Waits `ms` milliseconds before continuing.
+ *
+ * @param ms The number of milliseconds to sleep
+ * @returns A promise that resolves in `ms` milliseconds
+ */
+export function sleep (ms?: number): Promise<void> {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
